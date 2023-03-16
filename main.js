f7e9d3c00aed3e251c6340e88cba89c653bbd711
Azad <bahramali.az@gmail.com>
2023-03-16 20:39:53 +0100

    delete unused files

--- main.js
+++ main.js
@@ -1,47 +1,4 @@
-function changeColor(event) {
-    event.target.style.color = 'red';
-}
-
-let element = document.querySelectorAll("p2");
-
-console.log("el :")
-element.forEach(e => {
-    if (e.id.charAt(1) === '3') {
-        console.log("hummm this is 3")
-        e.style.color = "red";
-    }
-    console.log(e.id)
-});
-
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
-    }
-
-    img = document.getElementById("img1");
-
-    // Function to increase image size
-    function enlargeImg() {
-        img.style.transform = "scale(1.5)";
-        img.style.transition = "transform 0.25s ease";
-    }
-}
 
 
-/*
-    const showImageButton = document.getElementById("show-image-button");
-    const myImage = document.getElementById("my-image");
-    showImageButton.addEventListener("click", () => {
-    myImage.style.display = "block";
-});
-*/
+
+
